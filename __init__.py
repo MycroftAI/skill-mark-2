3900e5335a358e10e6abb238ce7181b8e3e72eb5
Åke Forslund <ake.forslund@gmail.com>
2019-05-06 14:52:03 +0200

    Remove on_gui_show_page handler when shutting down

--- __init__.py
+++ __init__.py
@@ -378,6 +378,8 @@ class Mark2(MycroftSkill):
                         self.on_handler_interactingwithuser)
         self.bus.remove('enclosure.mouth.viseme_list',
                         self.on_handler_speaking)
+        self.bus.remove('gui.page.show',
+                        self.on_gui_page_show)
         self.bus.remove('gui.page_interaction', self.on_gui_page_interaction)
         self.bus.remove('mycroft.mark2.register_idle', self.on_register_idle)
 
